5978eb3f0dd8d64b6e3cef046c3a056efa3e152f
nexuiz-cc <hyd524132502@gmail.com>
2024-11-20 23:32:03 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,109 @@
 <template>
-  <a-form
-    class="form"
-    :model="formState"
-    name="basic"
-    autocomplete="off"
-    @finish="onFinish"
-    @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-      class="item1"
-      v-model="username"
-      label="Username"
-      name="username"
-      :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username" class="input" />
-    </a-form-item>
+  <a-row class="parent">
+    <a-col :span="12"> left </a-col>
+    <a-col :span="12">
+      <a-tabs v-model:activeKey="activeKey" class="rightbox" centered>
+        <a-tab-pane key="1" tab="Sign Up">
+          <a-form
+            class="form"
+            :model="formState"
+            name="basic"
+            autocomplete="off"
+            @finish="onFinish"
+            @finishFailed="onFinishFailed"
+            :label-col="{ span: 8 }"
+            :wrapper-col="{ span: 16 }"
+          >
+            <a-form-item
+              class="item1"
+              v-model="signUpUsername"
+              label="ID"
+              name="username"
+              :rules="[{ required: true, message: 'Please input your username!' }]"
+            >
+              <a-input v-model:value="signUpState.username" class="input" />
+            </a-form-item>
 
-    <a-form-item
-      class="item2"
-      v-model="password"
-      label="Password"
-      name="password"
-      :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password" />
-    </a-form-item>
+            <a-form-item
+              class="item2"
+              v-model="signUpPassword"
+              label="Password"
+              name="signUpPassword"
+              :rules="[{ required: true, message: 'Please input your password!' }]"
+            >
+              <a-input-password v-model:value="signUpState.password" class="input" />
+            </a-form-item>
 
-    <a-form-item name="remember" class="item3">
-      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-    </a-form-item>
+            <a-form-item class="item4">
+              <a-button type="primary" html-type="submit" class="signupbtn">Sign Up</a-button>
+            </a-form-item>
+          </a-form>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="Sign In">
+          <a-form
+            class="form"
+            :model="formState"
+            name="basic"
+            autocomplete="off"
+            @finish="onFinish"
+            @finishFailed="onFinishFailed"
+            :label-col="{ span: 8 }"
+            :wrapper-col="{ span: 16 }"
+          >
+            <a-form-item
+              class="item1"
+              v-model="username"
+              label="ID"
+              name="username"
+              :rules="[{ required: true, message: 'Please input your username!' }]"
+            >
+              <a-input v-model:value="loginState.username" class="input" />
+            </a-form-item>
 
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }" class="item4">
-      <a-button type="primary" html-type="submit" class="btn">Submit</a-button>
-    </a-form-item>
-  </a-form>
+            <a-form-item
+              class="item2"
+              v-model="password"
+              label="Password"
+              name="password"
+              :rules="[{ required: true, message: 'Please input your password!' }]"
+            >
+              <a-input-password v-model:value="loginState.password" class="input" />
+            </a-form-item>
+
+            <a-form-item name="remember" class="item3">
+              <a-checkbox v-model:checked="loginState.remember">Remember me</a-checkbox>
+            </a-form-item>
+
+            <a-form-item class="item4">
+              <a-button type="primary"class="btn" @click="login()">Login</a-button>
+            </a-form-item>
+
+          </a-form>
+        </a-tab-pane>
+      </a-tabs>
+    </a-col>
+  </a-row>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import _ from 'lodash'
 import { getUserData } from './api/api'
-const formState = reactive({
+const activeKey = ref('1')
+const loginState = reactive({
   username: '',
   password: '',
   remember: true,
 })
-const onFinish = () => {
+const signUpState = reactive({
+  username: '',
+  password: '',
+})
+const login = () => {
   getUserData().then((res) => {
     const arr = res.data.userList
     const user = arr.find(
-      (user) => user.username === formState.username && user.password === formState.password,
+      (user) => user.username === loginState.username && user.password === loginState.password,
     )
     if (user) {
       alert('login success!')
@@ -67,41 +120,39 @@ const onFinishFailed = (errorInfo) => {
 
 <style scoped lang="css">
 .form {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(10, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-  margin-top: 320px;
+  width: 800px;
+  padding-top: 10%;
+  padding-right: 50%;
 }
-.item1 {
-  width: 300px;
-  justify-self: center;
-  grid-area: 4 / 2 / 5 / 3;
-  transform: scale(1.5);
+
+.parent {
 }
-.item2 {
-  transform: scale(1.5);
-  justify-self: center;
-  width: 300px;
-  grid-area: 5 / 2 / 6 / 3;
-}
-.item3 {
-  transform: scale(1.1);
-  margin-left: -250px;
-  width: 180px;
-  justify-self: center;
-  grid-area: 6 / 2 / 7 / 3;
-}
-.item4 {
-  transform: scale(1.33);
-  justify-self: center;
-  grid-area: 7 / 2 / 8 / 3;
-  margin-left: -135px;
-  position: absolute;
-  top: 730px;
+.rightbox {
+  margin-top: 100px;
+  margin-left: 220px;
+  width: 500px;
 }
 .btn {
   width: 150px;
+  position: relative;
+  left: 200px;
+  top: -20px;
+}
+.signupbtn{
+  width: 150px;
+  position: relative;
+  left: 200px;
+  top: 10px;
+}
+.item1 {
+  font-size: 20px;
+}
+.input {
+  width: 200px;
+}
+.item3 {
+  position: relative;
+  right: -75px;
+  top: -20px;
 }
 </style>
